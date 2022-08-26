a772bb46e84eae267394c2d97bc4b26374dee638
VeraPandi <84042427+VeraPandi@users.noreply.github.com>
2022-08-27 01:29:30 +0200

    Add: 404 page route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+   BrowserRouter as Router,
+   Routes,
+   Route,
+   Navigate,
+} from "react-router-dom";
 import ThemeContextProvider from "./context/ThemeContext";
 import Home from "./pages/Home";
 import Portfolio from "./pages/Portfolio";
 import Project from "./pages/Project";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 import Navigation from "./containers/Navigation";
 import Sidebar from "./containers/Sidebar";
 import "./styles/main.scss";
@@ -12,7 +18,7 @@ import "./styles/main.scss";
 function App() {
    return (
       <ThemeContextProvider>
-         <Router>
+         <Router basename={process.env.PUBLIC_URL}>
             <Navigation />
             <Routes>
                <Route path="/" element={<Home />} />
@@ -20,6 +26,8 @@ function App() {
                <Route path="/Project/:ID" element={<Project />} />
                <Route path="/About" element={<About />} />
                <Route path="/Contact" element={<Contact />} />
+               <Route path="/404" element={<NotFound />} />
+               <Route path="*" element={<Navigate replace to="/404" />} />
             </Routes>
             <Sidebar />
          </Router>
