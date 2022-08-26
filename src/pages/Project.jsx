5c778e9fa3b100df20da64a15c9af55d27139912
VeraPandi <84042427+VeraPandi@users.noreply.github.com>
2022-08-27 00:12:35 +0200

    Test: Deployment

--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -17,6 +17,7 @@ const Project = () => {
 
    const { ID } = useParams();
    const work = worksList.find((element) => element.id === ID);
+   console.log(work.gallery.map((image) => image));
 
    return (
       <main className="page-main">
@@ -25,7 +26,31 @@ const Project = () => {
             text={`title-text ${theme}`}
             underline={`underline ${theme}`}
          />
-         {work.intro}
+
+         <article>
+            <div className="picture">
+               <img src={work.cover} alt="" />
+               {/* "/images/photo.png" */}
+            </div>
+            <h2 className="Projet-title">{work.title}</h2>
+            <h3 className="completion-time">
+               {`Temps de réalisation : ${work.time}`}
+            </h3>
+            <span className="context">{`Cadre du projet : ${work.type}`}</span>
+            <p className="description">{work.intro}</p>
+
+            <section className="gallery">
+               <div className="gallery-content">
+                  {work.gallery.map((image, index) => (
+                     <div className="image" key={`${image}-${index}`}>
+                        <img src={image} alt="" />
+                     </div>
+                  ))}
+               </div>
+            </section>
+
+            <span className="tools">{`Réalisé avec : ${work.techs}`}</span>
+         </article>
       </main>
    );
 };
