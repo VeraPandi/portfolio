2afe0bf10ce42f6b7123f1b537f4a49a3c7fc9f8
VeraPandi <84042427+VeraPandi@users.noreply.github.com>
2022-12-16 04:25:44 +0100

    Add: Additional E2E tests for '404' page

--- cypress/e2e/notFound-page.cy.jsx
+++ cypress/e2e/notFound-page.cy.jsx
@@ -1,30 +1,44 @@
 import visit from "cypress/react18";
 
 describe("404 page", () => {
-   it("Redirection vers la page 404 quand l'url est incorrecte", () => {
+   it("Redirection to the 404 page when the url is incorrect", () => {
       cy.visit("/portfolio/doesNotExist");
       cy.location("pathname").should("eq", "/portfolio/404");
    });
 
-   context("Tous les éléments de la page sont visibles", () => {
-      it("Un bouton de mode d'affichage", () => {
+   context("Elements visible on the page:", () => {
+      it("A display theme button", () => {
          cy.get('[data-cy="mode-btn"]');
       });
 
-      it("Une navigation", () => {
-         cy.get('[data-cy="navigation"]');
+      context("Some error messages", () => {
+         it("A '404' number", () => {
+            cy.get(".error-code").should("have.text", "404");
+         });
+
+         it("A text", () => {
+            cy.get(".error-message").should(
+               "have.text",
+               "Oups ! La page que vous recherchez semble introuvable..."
+            );
+         });
       });
 
-      it("Un message d'erreur", () => {
-         cy.get(".error-code").should("have.text", "404");
-         cy.get(".error-message").should(
-            "have.text",
-            "Oups ! La page que vous recherchez semble introuvable..."
-         );
-      });
-
-      it("Un footer", () => {
+      it("A footer", () => {
          cy.get(".footer");
+      });
+
+      context("Navigation with:", () => {
+         it("A link to return to the home page", () => {
+            cy.get('[data-cy="navigation"] > a').first().click();
+            cy.location("pathname").should("eq", "/portfolio/");
+         });
+
+         it("A link to return to the 'projects' page", () => {
+            cy.visit("/portfolio/404");
+            cy.get('[data-cy="navigation"] > a').last().click();
+            cy.location("pathname").should("eq", "/portfolio/Projects");
+         });
       });
    });
 });
